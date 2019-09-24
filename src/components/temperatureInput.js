f441272bb2cb4d2284e01e55f0099aa639167a56
heyong <macx@macx.cn>
2019-09-24 14:33:23 +0800

    增加了嵌入的canvas

--- src/components/temperatureInput.js
+++ src/components/temperatureInput.js
@@ -4,26 +4,31 @@ const scaleNames = {
   f: 'Fahrenheit'
 }
 class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {temperature: ''};
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = { temperature: '' }
+    this.canvas = React.createRef()
   }
 
-  handleChange(e) {
+  handleChange (e) {
     this.props.onTemperatureChange(e.target.value) // 反向回调，回调到父级查函数
   }
 
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
+  render () {
+    const temperature = this.props.temperature
+    const scale = this.props.scale
     return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
-               onChange={this.handleChange} />
-      </fieldset>
-    );
+      <div>
+        <fieldset>
+          <legend>Enter temperature in {scaleNames[scale]}:</legend>
+          <input
+            value={temperature}
+            onChange={this.handleChange}
+          />
+        </fieldset>
+      </div>
+    )
   }
 }
 export default TemperatureInput
--- src/views/assembly.js
+++ src/views/assembly.js
@@ -1,57 +1,62 @@
 import React from 'react'
 import FancyBorder from '../components/fancyBorder'
 import Dialog from '../components/dialog'
+import Canvas from '../components/canvas'
 import { bindActionCreators } from 'redux'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { addItem, submitInput } from '../actions'
 
-
 class Assembly extends React.Component { // 这个组件训练组价嵌套
-  addTodo() {
+  addTodo () {
     this.props.addItem()
     console.log('查看当前输出的props--->', this.props)
   }
-  submitInput(value) {
+
+  submitInput (value) {
     this.props.submitInput(value)
   }
+
   render () {
-    return(
+    return (
       <div>
         <p>{this.props.list.addNums.list}</p>
-        <input type = "button" value = "点击按钮" onClick = {() => {this.addTodo()}}/>
-        <input type = "text" placeholder = '输入信息提交会直接给到状态树' onKeyDown={(e) => {
-            if (e.keyCode === 13){
-              let title = e.target.value
+        <input type='button' value='点击按钮' onClick={() => { this.addTodo() }} />
+        <input
+          type='text' placeholder='输入信息提交会直接给到状态树' onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              const title = e.target.value
               if (title.length) {
                 this.submitInput(title)
-              } 
+              }
             }
-          }}/>
-        <FancyBorder color="blue">
-          <h1 className="Dialog-title">
+                    }}
+        />
+        <FancyBorder color='blue'>
+          <h1 className='Dialog-title'>
             Welcome!!!!
           </h1>
-          <p className="Dialog-message">
+          <p className='Dialog-message'>
             Thank you for visiting our spacecraft!!!!
           </p>
         </FancyBorder>
         <Dialog
-        title="Welcome"
-        message="Thank you for visiting our spacecraft!" />
+          title='Welcome'
+          message='Thank you for visiting our spacecraft!'
+        />
+        <Canvas />
       </div>
     )
   }
 }
 
-function mapDispatchToProps(dispatch) { // 拿到提交方法
+function mapDispatchToProps (dispatch) { // 拿到提交方法
   return {
     addItem: bindActionCreators(addItem, dispatch),
     submitInput: bindActionCreators(submitInput, dispatch)
   }
 }
 
-
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     // list: state.addNums.num  // 会返回这个函数名的对象
     list: state
